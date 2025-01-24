4001a49676fdc8c6360c10ea896b8d3d4a706f4a
Rivindhu Geeneth <rivindhu98@gmail.com>
2025-01-24 13:26:57 +0530

    Added the files LICENSE and README.md and also add the necessary info for setup.py file.

---
+++ README.md
@@ -0,0 +1,16 @@
+# djadminshield
+
+djangoadminshield library is for secure the default django admin dashboard from hackers.
+
+## Installation
+
+```bash
+pip install djadminshield
+```
+
+## Usage
+Inside your project urls.py file add this line of code.
+
+```python
+pass
+```
--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="djadminshield",
     version="0.1.0",
+    author='Kudamage Rivindhu Venuka Geeneth',
+    author_email='rivindhu98@gmail.com',
+    description='A Django library to secure the admin panel by faking login pages and logging attempts.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Rivindhu/djadminshield',
     packages=find_packages(),
     include_package_data=True,
     install_requirements=[
@@ -13,4 +19,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
     ],
+    install_requires=[
+        'PyYAML==6.0.2',
+        'ua-parser==1.0.0',
+        'ua-parser-builtins==0.18.0.post1',
+        'user-agents==2.2.0',
+        ],
+    license='MIT',
 )
